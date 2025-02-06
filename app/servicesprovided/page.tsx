5e02c4fc1ce7208be069fe3972eaf1f3e48084f2
franss29 <fransiskusagung486@gmail.com>
2025-02-06 21:41:21 +0700

    test

--- app/servicesprovided/page.tsx
+++ app/servicesprovided/page.tsx
@@ -1,12 +1,12 @@
 import Header from "@/components/main/header";
-import Hero from "@/components/aboutus/hero";
+
 
 export default function Services(){
   return(
     <>
       <Header/>
       <main className="min-h-screen bg-white">
-        <Hero />
+        
       </main>
     </>
   )
